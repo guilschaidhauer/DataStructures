1719f66a33f0289b114960317a31edab675fdb8d
guilschaidhauer <guilherme.schaidhauer.de.castro@sap.com>
2017-07-22 13:29:43 -0300

    Clean Source.cpp

--- DataStructures/Source.cpp
+++ DataStructures/Source.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
-#include "LinkedList.h"
+//#include "LinkedList.h"
 //#include "BinarySearchTree.h"
-#include "Hashtable.h"
+// #include "Hashtable.h"
 
 using namespace std;
 
 int main()
 {
-	Hashtable myHashTable;
-	
-	Item item1;
-	item1.name = "Guilherme";
-	item1.phoneNumber = "1234";
-
-	Item item2;
-	item2.name = "Guilherme";
-	item2.phoneNumber = "5678";
-
-	myHashTable.add(item1);
-	myHashTable.add(item2);
-
-	myHashTable.remove(item2);
-
-	cout << myHashTable.toString() << endl;
-
 	system("pause");
 	return 0;
 }
